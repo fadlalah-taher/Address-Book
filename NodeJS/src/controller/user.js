724715a3d97884ae83a1662fa53de07bb7178b51
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-23 23:24:14 +0300

    Adding function get on user.js

--- NodeJS/src/controller/user.js
+++ NodeJS/src/controller/user.js
@@ -3,6 +3,28 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
 
+
+
+async function get(req, res) {
+  try {
+    console.log(req.query);
+
+    if (req.query.id) { // ?id=k1231 -> query paramet
+      const id = req.query.id;
+      const result = await getById(id);
+      console.log('result of specific user =>', result);
+      return res.send(result);
+    }
+
+    const result = await getUsers();
+    console.log('result =>', result);
+
+    return res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function register(req, res) {
     try {
       console.log(req.body);
