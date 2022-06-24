a2aa2786ad2bf4daee319435f96d72c7f5a88d04
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-24 20:01:17 +0300

    Adding Register route

--- address-book/src/App.js
+++ address-book/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path='/' element={<><Login /></>}> </Route>
+      <Route path='/Register' element={<><Register /></>}> </Route>
       </Routes>
     </BrowserRouter>
   );
