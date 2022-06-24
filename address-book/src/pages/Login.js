0ce6f712d1e0e43d643fda99bb43c195b23bda6d
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-24 21:21:28 +0300

    Adding axios in onLogin

--- address-book/src/pages/Login.js
+++ address-book/src/pages/Login.js
@@ -10,10 +10,26 @@ const Login = () => {
 
     const [field, setField] = useState(false);
 
+    async function onLogin(e){
+        e.preventDefault();
+        let item = {email,password};
+  
+        axios({
+          method: 'post',
+          url: 'http://localhost:3000/api/user/login',
+          data: item,
+      }).then(function (response) {
+        console.log(response);
+      }).catch(function(response){
+        setField(true);
+        console.log(response);
+    })
+    }
+
   return (
     <div className='body'>
       <div className='form-container'>
-        <form className="add-form">
+        <form className="add-form" onSubmit={onLogin}>
           <h1>Login</h1>
           <div className="input-field">
             <input
