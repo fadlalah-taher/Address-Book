c4fc91e270b2f1391dcd37bdd46928b349185cde
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-24 09:26:15 +0300

    Adding getContacts on contact.js

--- NodeJS/src/controller/contact.js
+++ NodeJS/src/controller/contact.js
@@ -3,13 +3,26 @@ const Contact = require("../../model/Contact");
 async function addContacts(req, res){
     try{
         const {fullname,  email, number, relationStatus, location} = req.body;
-        const contact = await new Contact({ fullname, email, number, relationStatus, location, "user": req.user});
+        console.log(req.query);
+        console.log(req.user);
+        //console.log(req);
+        const contact = await new Contact({ fullname, email, number, relationStatus, location, "userId": req.user});
         const result = await contact.save();
         
         return res.send({"success": true, "results": result});
     }catch (error) {
         console.log(error);
-      }
+    }
 }
 
-module.exports = {addContacts};
+async function getContacts(req, res){
+    try{
+        const contacts = await Contact.find({'userId': req.userId.id});
+        return res.send({"success": true, "results": contacts})
+    }catch (error) {
+        console.log(error);
+    }
+    
+}
+
+module.exports = {addContacts, getContacts};
--- NodeJS/src/index.js
+++ NodeJS/src/index.js
@@ -1,11 +1,12 @@
 const {Router} = require('express');
 const {register, login} = require('./controller/user');
-const {addContacts} = require('./controller/contact');
+const {addContacts, getContacts} = require('./controller/contact');
 const router = Router();
 
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/addContacts', addContacts);
+router.post('/getContacts', getContacts);
 
 module.exports = router;
