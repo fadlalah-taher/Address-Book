dd1d16a8c518ae92838ca73c39d5328259462217
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-25 10:40:28 +0300

    Creating AddContact.js in pages

---
+++ address-book/src/pages/AddContact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+
+const AddContact = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddContact
