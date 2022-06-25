69dbeba3efc83d048ce2e287d112e14d84144dbf
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-25 20:10:07 +0300

    Adding axiox inside deleteContact

--- address-book/src/components/Contact.js
+++ address-book/src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 import { IconContext } from 'react-icons';
-import {AiOutlineMail} from "react-icons/ai";
+import {AiOutlineMail,AiFillDelete} from "react-icons/ai";
 import {BsFillTelephoneFill} from "react-icons/bs";
 import {GiRelationshipBounds} from "react-icons/gi";
 
@@ -14,7 +14,7 @@ const Contact = () => {
     const fetchContact = async () => {
         // Display Contact
         try{
-            await axios.post(`http://localhost:3000/contact/getContactById?id=62b5ba77b634c29f22cd7463`)//,{id: "62b5ba77b634c29f22cd7463"}
+            await axios.post(`http://localhost:3000/contact/getContactById?id=62b59d8261567545e34ea3f9`)//,{id: "62b5ba77b634c29f22cd7463"}
             .then(res =>{
                 const data = res.data['results'][0];
                 console.log(res.data['results'][0]);
@@ -27,6 +27,23 @@ const Contact = () => {
         }
     }
 
+    // Delete Contact
+    const deleteContact = async () => {
+        try{
+            await axios.post(`http://localhost:3000/contact/deleteContact?id=62b59d8261567545e34ea3f9`)//,{id: "62b5ba77b634c29f22cd7463"}
+            .then(res =>{
+                console.log(res);
+                window.location = "/";
+                
+                //const mydata = res.data;
+                //setOptions(mydata['options']);
+            })
+        } catch(err){
+            console.log(err);
+        }
+    }
+  
+
 
     useEffect(() => {
         fetchContact();
@@ -34,12 +51,19 @@ const Contact = () => {
   return (
     <IconContext.Provider value={{color:'rgb(239, 79, 95)'}}>
     <div className='body'>
-        <div className='contact-container'>
-            <div>{contact.fullname}</div>
-            <div><AiOutlineMail/>  {contact.email}</div>
-            <div><BsFillTelephoneFill/>{contact.number}</div>
-            <div><GiRelationshipBounds/>{contact.relationStatus}</div>
-            <div>Location</div>
+        <div className='contact'>
+            <div className='contact-container'>
+                <div className='contact-left'>
+                    <div>{contact.fullname}</div>
+                    <div><AiOutlineMail/>  {contact.email}</div>
+                    <div><BsFillTelephoneFill/>{contact.number}</div>
+                    <div><GiRelationshipBounds/>{contact.relationStatus}</div>
+                </div>
+                <div className='contact-right'>
+                    <div>Location</div>
+                    <div ><AiFillDelete onClick={deleteContact} className="deleteIcon"/>Delete Contact</div>
+                </div>
+            </div>
         </div>
     </div>
     </IconContext.Provider>
