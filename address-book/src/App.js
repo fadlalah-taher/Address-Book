ba59b471fbf90cba6d4254b078861b1ee7467e54
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-25 08:58:05 +0300

    Adding Contacts Route

--- address-book/src/App.js
+++ address-book/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Contacts from './pages/Contacts';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
       <Route path='/' element={<><Login /></>}> </Route>
       <Route path='/Register' element={<><Register /></>}> </Route>
+      <Route path='/Contacts' element={<><Contacts /></>}> </Route>
       </Routes>
     </BrowserRouter>
   );
