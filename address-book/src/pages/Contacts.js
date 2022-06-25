f25cd9e2f09923a065ad63d7aea97c1557c32aee
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-25 21:30:59 +0300

    Adding axios inside fetchContacts

--- address-book/src/pages/Contacts.js
+++ address-book/src/pages/Contacts.js
@@ -7,7 +7,25 @@ const Contacts = () => {
     var userId = localStorage.getItem("access_token");
     console.log(userId);
 
-    
+    const fetchContacts = async () => {
+        // Display Contact
+        try{
+            await axios.post(`http://localhost:3000/contact/getContactsByUserId?id=${userId}`)//,{id: "62b5ba77b634c29f22cd7463"}
+            .then(res =>{
+                const data = res.data['results'];
+                console.log(res.data['results']);
+                setContacts(data);
+            })
+        } catch(err){
+            //console.log(err);
+        }
+    }
+
+
+console.log(contacts);
+    useEffect(() => {
+        fetchContacts();
+    }, []);
   return (
     <div className='body'>
         <div className='search'>
