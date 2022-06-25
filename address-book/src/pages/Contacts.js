a09f34090782d3440be03ecba14b96ec9c9e64dc
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-25 21:29:59 +0300

    Adding on Contact.js

--- address-book/src/pages/Contacts.js
+++ address-book/src/pages/Contacts.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Contacts = () => {
+    const [contacts, setContacts] = useState("");
+    var userId = localStorage.getItem("access_token");
+    console.log(userId);
+
+    
   return (
-    <div>
-      Hello
+    <div className='body'>
+        <div className='search'>
+            <input placeholder={"Name"}/>
+            <input placeholder={"Email"}/>
+            <input placeholder='relation'/>
+        </div>
+        <div className='contacts-container'>   
+            <div>contact.fullname</div>
+            <div> contact.email</div>
+            <div>contact.number</div>
+            <div>contact.relationStatus</div>
+            <div>Location</div>  
+        </div>
     </div>
   )
 }
