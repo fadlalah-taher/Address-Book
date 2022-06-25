93ea6f92b4fc080cd52415bbd85a9f3a32e8c7e3
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-25 19:15:40 +0300

    Adding axiox inside fetchContact

--- address-book/src/components/Contact.js
+++ address-book/src/components/Contact.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+
 import { IconContext } from 'react-icons';
 import {AiOutlineMail} from "react-icons/ai";
 import {BsFillTelephoneFill} from "react-icons/bs";
-import {GiRelationshipBounds} from "react-icons/gi"
+import {GiRelationshipBounds} from "react-icons/gi";
+
+import axios from 'axios';
+
 const Contact = () => {
+
+    const [contact, setContact] = useState("");
+    const fetchContact = async () => {
+        // Display Contact
+        try{
+            await axios.post(`http://localhost:3000/contact/getContactById?id=62b5ba77b634c29f22cd7463`)//,{id: "62b5ba77b634c29f22cd7463"}
+            .then(res =>{
+                const data = res.data['results'][0];
+                console.log(res.data['results'][0]);
+                setContact(data);
+                //const mydata = res.data;
+                //setOptions(mydata['options']);
+            })
+        } catch(err){
+            //console.log(err);
+        }
+    }
+
+
+console.log(contact.fullname);
+    useEffect(() => {
+        fetchContact();
+    }, []);
   return (
     <IconContext.Provider value={{color:'rgb(239, 79, 95)'}}>
     <div className='body'>
         <div className='contact-container'>
-            <div>Fadlalah</div>
-            <div><AiOutlineMail/>  fadel@gmail.com</div>
-            <div><BsFillTelephoneFill/>76 333450</div>
-            <div><GiRelationshipBounds/>Married</div>
+            <div>{contact.fullname}</div>
+            <div><AiOutlineMail/>  {contact.email}</div>
+            <div><BsFillTelephoneFill/>{contact.number}</div>
+            <div><GiRelationshipBounds/>{contact.relationStatus}</div>
             <div>Location</div>
         </div>
     </div>
