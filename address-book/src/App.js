60ca76ef05ff050deed06e7aee1a1dfa5f11f9b9
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-25 11:05:11 +0300

    Adding AddContact Route

--- address-book/src/App.js
+++ address-book/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Contacts from './pages/Contacts';
+import AddContact from './pages/AddContact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={<><Login /></>}> </Route>
       <Route path='/Register' element={<><Register /></>}> </Route>
       <Route path='/Contacts' element={<><Contacts /></>}> </Route>
+      <Route path='/AddContact' element={<><AddContact /></>}> </Route>
       </Routes>
     </BrowserRouter>
   );
