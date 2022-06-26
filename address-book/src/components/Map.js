5efc9cdfa4355acffb4a1887e038b897eef8246c
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-26 11:36:58 +0300

    Updating and adding LocalStorage

--- address-book/src/components/Map.js
+++ address-book/src/components/Map.js
@@ -3,44 +3,51 @@ import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaf
 
 import '../App.css';
 import { useState } from "react";
+import AddContact from '../pages/AddContact';
 
-const Map = () => {
+
+const Map = ({latlng}) => {
 
     const [lat, setLat] = useState(33.884211);
     const [lag, setLag] = useState(35.492409);
    
     const [position, setPosition] = useState(null);
-
+    console.log("Helllo");
     // Events
     const HandleClickMap = () =>{
         const map = useMapEvents({
             click(e){
                 setPosition(e.latlng);
                 map.flyTo(e.latlng)
+                console.log(position.lat);
+                console.log(position.lng);
+                localStorage.setItem("coordinateLat", position.lat);
+                localStorage.setItem("coordinateLng", position.lng);
             }
         })
         return position== null
         ? null 
         : <Marker position={position}></Marker>;
     };
-    console.log(position);
+    
 
     
   return (
     <div>
-        {position == null ? null : <> <p>"lat:" {position.lat} </p> <p>"lag:" {position.lng} </p></>}
-        <MapContainer center={[lat, lag]} zoom={13}  scrollWheelZoom={true}>
+        {/* {position == null ? null : <> <p>"lat:" {position.lat} </p> <p>"lag:" {position.lng} </p>/ */}
+        <MapContainer center={[lat, lag]} zoom={5}  scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <HandleClickMap />
-            <Marker position={[33.884211, 35.492409]}>
+            {/* <Marker position={[33.884211, 35.492409]}>
                 <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
-            </Marker>
+            </Marker> */}
         </MapContainer> 
+        {/* <AddContact/> */}
     </div>
   )
 }
