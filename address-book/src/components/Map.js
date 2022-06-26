b346744e80063be568eb76d21766b9b2a6812ae0
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-26 09:32:15 +0300

    Adding HandleClickMap

--- address-book/src/components/Map.js
+++ address-book/src/components/Map.js
@@ -9,24 +9,38 @@ const Map = () => {
     const [lat, setLat] = useState(33.884211);
     const [lag, setLag] = useState(35.492409);
    
-    // const [position, setPosition] = useState(null);
+    const [position, setPosition] = useState(null);
+
+    // Events
+    const HandleClickMap = () =>{
+        const map = useMapEvents({
+            click(e){
+                setPosition(e.latlng);
+                map.flyTo(e.latlng)
+            }
+        })
+        return position== null
+        ? null 
+        : <Marker position={position}></Marker>;
+    };
+    console.log(position);
 
     
   return (
     <div>
-        
-    <MapContainer center={[lat, lag]} zoom={13}  scrollWheelZoom={true}>
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* <HandleClickMap /> */}
-        <Marker position={[33.884211, 35.492409]}>
-            <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-        </Marker>
-    </MapContainer> 
+        {position == null ? null : <> <p>"lat:" {position.lat} </p> <p>"lag:" {position.lng} </p></>}
+        <MapContainer center={[lat, lag]} zoom={13}  scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <HandleClickMap />
+            <Marker position={[33.884211, 35.492409]}>
+                <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer> 
     </div>
   )
 }
