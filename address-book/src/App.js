0d3e2f5e0096da30ac9fec41882bbd7196502f90
fadlalah-taher <fadeltaher111@gmail.com>
2022-07-14 10:27:44 +0300

    Adding UpdateContant Route

--- address-book/src/App.js
+++ address-book/src/App.js
@@ -5,6 +5,7 @@ import Register from './pages/Register';
 import Contacts from './pages/Contacts';
 import AddContact from './pages/AddContact';
 import Contact from './components/Contact';
+import UpdateContact from './pages/UpdateContact';
 import Map from './components/Map';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path='/' element={<><Login /></>}> </Route>
       <Route path='/Register' element={<><Register /></>}> </Route>
       <Route path='/Contacts' element={<><Contacts /></>}> </Route>
+      <Route path='/UpdateContact' element={<><UpdateContact /></>}> </Route>
       <Route path='/AddContact' element={<><AddContact /></>}> </Route>
       <Route path='/Contact' element={<><Contact /></>}> </Route>
       <Route path='/Map' element={<><Map /></>}> </Route>
